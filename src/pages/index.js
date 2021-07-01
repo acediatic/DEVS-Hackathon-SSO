8e587d5017b7a386614d67e69d964ba2ea2c2381
Adam Sinclair <hello@adamsinclair.dev>
2021-07-02 11:20:44 +1200

    removed seo files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react"
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 import Hero from "../components/hero"
 
 const IndexPage = () => (
   <Layout>
-    <SEO />
     <Hero />
   </Layout>
 )
